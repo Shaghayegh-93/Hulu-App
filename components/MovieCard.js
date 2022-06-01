4721870c18ffb9acbc8387c43d34789e32d8785f
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-01 17:11:04 +0430

    add MovieCard component for every single movie

---
+++ components/MovieCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MovieCard = ({ result }) => {
+  console.log("result:", result);
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+};
+
+export default MovieCard;

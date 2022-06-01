259b31ac11281308f76fd7536a8749b66d0ab72b
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-01 16:40:36 +0430

    change style attribute

--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -3,7 +3,7 @@ import React from "react";
 const HeaderItem = ({ title, Icon }) => {
   return (
     <div className="group flex flex-col items-center cursor-pointer w-12 sm:w-20 hover:text-white">
-      <Icon className="h-8 mb-1 group-hover:animate-bounce  " />
+      <Icon className="h-6 sm:h-7 mb-1  group-hover:animate-bounce  " />
 
       <p className="tracking-widest opacity-0 group-hover:opacity-100">
         {title}

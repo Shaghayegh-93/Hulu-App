b1905f1cf0c83f7159e2a34c94dfe60d6835c16a
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-02 13:34:25 +0430

    import react-flip-move

--- components/Results.js
+++ components/Results.js
@@ -1,14 +1,15 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import FlipMove from "react-flip-move";
 
 const Results = ({ results }) => {
   console.log("results:", results);
   return (
-    <div>
-      {results.results.map((result) => (
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+      {results.map((result) => (
         <MovieCard key={result.id} result={result} />
       ))}
-    </div>
+    </FlipMove>
   );
 };
 

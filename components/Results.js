a8e7231e06c6d73f85162160c9c48e3f426b5923
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-05 14:36:44 +0430

    remove FlipMove tag

--- components/Results.js
+++ components/Results.js
@@ -1,15 +1,15 @@
 import React from "react";
 import MovieCard from "./MovieCard";
-import FlipMove from "react-flip-move";
+// import FlipMove from "react-flip-move";
 
 const Results = ({ results }) => {
-  console.log("results:", results);
+  // console.log("results:", results);
   return (
-    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex 3xl:flex-wrap 3xl:justify-center">
       {results.map((result) => (
         <MovieCard key={result.id} result={result} />
       ))}
-    </FlipMove>
+    </div>
   );
 };
 

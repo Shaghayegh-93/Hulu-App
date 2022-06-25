10d120fb2afcd4ebf5cee7dc84c7af533b3fdabb
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-25 21:21:20 +0430

    remove unusable import

--- components/Results.js
+++ components/Results.js
@@ -1,9 +1,7 @@
 import React from "react";
 import MovieCard from "./MovieCard";
-// import FlipMove from "react-flip-move";
 
 const Results = ({ results }) => {
-  // console.log("results:", results);
   return (
     <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex 3xl:flex-wrap 3xl:justify-center">
       {results.map((result) => (
